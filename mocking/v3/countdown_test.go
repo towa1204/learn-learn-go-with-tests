553e3b5b7c7aa91472c3ada991468c66e7a1b37b
towa1204 <lsadsfj@gmail.com>
2023-05-24 23:46:14 +0900

    add mocking

---
+++ mocking/v3/countdown_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+// Sleep()関数を持つモック
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func TestCountdown(t *testing.T) {
+	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
+
+	Countdown(buffer, spySleeper)
+
+	got := buffer.String()
+	want := `3
+2
+1
+Go!`
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+
+	if spySleeper.Calls != 3 {
+		t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
+	}
+}
