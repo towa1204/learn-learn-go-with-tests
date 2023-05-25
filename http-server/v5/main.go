9bd86b439aad01d4ba8c9afea9af47171fe08c25
towa1204 <lsadsfj@gmail.com>
2023-05-25 22:58:54 +0900

    add http-server v5

---
+++ http-server/v5/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type InMemoryPlayerStore struct{}
+
+// スタブ
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
+func (i *InMemoryPlayerStore) RecordWin(name string) {}
+
+func main() {
+	server := &PlayerServer{&InMemoryPlayerStore{}}
+	log.Fatal(http.ListenAndServe(":5000", server))
+}
